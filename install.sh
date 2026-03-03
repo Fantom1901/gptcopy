f1f4a7bf2c6f639533c5ceb57ee3e2be8dea2823
fantom <tim19011974tim@gmail.com>
2026-03-03 16:21:58 +0700

    Initial realease v3.0

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+INSTALL_DIR="$HOME/.local/bin"
+mkdir -p "$INSTALL_DIR"
+
+echo ":: Загрузка gptcopy..."
+# Путь к твоему будущему репозиторию (замени USERNAME на свой)
+curl -sSL "https://github.com/Fantom1901/gptcopy/main/bin/gptcopy" -o "$INSTALL_DIR/gptcopy"
+chmod +x "$INSTALL_DIR/gptcopy"
+
+# Определение Shell и настройка PATH
+SHELL_TYPE=$(basename "$SHELL")
+case "$SHELL_TYPE" in
+    fish) CONF="$HOME/.config/fish/config.fish";;
+    zsh)  CONF="$HOME/.zshrc";;
+    *)    CONF="$HOME/.bashrc";;
+esac
+
+if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]]; then
+    echo ":: Добавление $INSTALL_DIR в PATH..."
+    if [ "$SHELL_TYPE" == "fish" ]; then
+        echo "fish_add_path $INSTALL_DIR" >> "$CONF"
+    else
+        echo "export PATH=\"\$PATH:$INSTALL_DIR\"" >> "$CONF"
+    fi
+    echo ":: Перезапустите терминал или выполните 'source $CONF'"
+fi
+
+echo ":: Установка завершена. gptcopy готов к работе!"
