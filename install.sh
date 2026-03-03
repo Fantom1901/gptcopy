07c5235bf75224f3cdb2363cb2f7ac53488b65fe
Fantom1901 <tim19011974tim@gmail.com>
2026-03-03 16:32:50 +0700

    Update gptcopy download link and installation message

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@ INSTALL_DIR="$HOME/.local/bin"
 mkdir -p "$INSTALL_DIR"
 
 echo ":: Загрузка gptcopy..."
-# Путь к твоему будущему репозиторию (замени USERNAME на свой)
-curl -sSL "https://https://github.com/Fantom1901/gptcopy/bin" -o "$INSTALL_DIR/gptcopy"
+# ПРОВЕРЬ ЭТУ ССЫЛКУ:
+curl -sSL "https://raw.githubusercontent.com/Fantom1901/gptcopy/main/bin/gptcopy" -o "$INSTALL_DIR/gptcopy"
 chmod +x "$INSTALL_DIR/gptcopy"
 
-# Определение Shell и настройка PATH
 SHELL_TYPE=$(basename "$SHELL")
 case "$SHELL_TYPE" in
     fish) CONF="$HOME/.config/fish/config.fish";;
@@ -24,7 +23,6 @@ if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]]; then
     else
         echo "export PATH=\"\$PATH:$INSTALL_DIR\"" >> "$CONF"
     fi
-    echo ":: Перезапустите терминал или выполните 'source $CONF'"
 fi
 
-echo ":: Установка завершена. gptcopy готов к работе!"
+echo ":: Установка завершена. gptcopy готов!"
